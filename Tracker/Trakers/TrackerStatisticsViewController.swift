6b2575429e097c03e6d8ce8b375729a853e46dad
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-04-20 00:20:15 +0300

    Добавлен TrackerStatisticsViewController

---
+++ Tracker/Trakers/TrackerStatisticsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TrackerStatisticsViewController.swift
+//  Tracker
+//
+//  Created by Алина on 19.04.2025.
+//
+import UIKit
+
+final class TrackerStatisticsViewController: UIViewController {
+    //TO DO:
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
