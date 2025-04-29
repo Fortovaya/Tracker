edc908082b72d26a5cfaa6507f82de6f7b37a2bc
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-04-29 11:23:17 +0300

    Сверстан MainTabBarController

--- Tracker/MainTabBarController/MainTabBarController.swift
+++ Tracker/MainTabBarController/MainTabBarController.swift
@@ -8,35 +8,64 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
+    private enum Tab: Int {
+        case trackers = 0
+        case statistics = 1
+        
+        var title: String {
+            switch self {
+            case .trackers: return "Трекеры"
+            case .statistics: return "Статистика"
+            }
+        }
+        
+        var imageName: String {
+            switch self {
+            case .trackers: return "trackers"
+            case .statistics: return "hare"
+            }
+        }
+        
+        var viewController: UIViewController {
+            switch self {
+            case .trackers:
+                return TrackerViewController()
+            case .statistics:
+                return TrackerStatisticsViewController()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureTabBarControllers()
+        
+        viewControllers = [
+            makeTab(for: .trackers),
+            makeTab(for: .statistics)
+        ]
+        
         setupMainTabBarController()
     }
     
-    private func configureTabBarControllers(){
-        let trackersVC = TrackerViewController()
-        let trackersNavVC = UINavigationController(rootViewController: trackersVC)
-        trackersNavVC.tabBarItem = UITabBarItem(title: "Трекеры", image: UIImage(named: "trackers"), tag: 0)
-        
-        let trackerStatisticsVC = TrackerStatisticsViewController()
-        let trackerStatisticNavVc = UINavigationController(rootViewController: trackerStatisticsVC)
-        trackerStatisticNavVc.tabBarItem = UITabBarItem(title: "Статистика", image: UIImage(named: "hare"), tag: 1)
-        
-        viewControllers = [trackersNavVC, trackerStatisticNavVc]
-        
-        setupNavigationBar(for: trackersNavVC)
-        setupNavigationBar(for: trackerStatisticNavVc)
-    }
-    
-    private func setupMainTabBarController(){
+    private func setupMainTabBarController() {
         view.backgroundColor = .ypWhite
         tabBar.isTranslucent = false
         tabBar.tintColor = .ypBlue
         tabBar.unselectedItemTintColor = .ypGray
     }
     
-    private func setupNavigationBar(for navigationController: UINavigationController){
-        navigationController.navigationBar.isHidden = true
+    private func makeTab(for tab: Tab) -> UINavigationController {
+        let viewController = tab.viewController
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        navigationController.tabBarItem = UITabBarItem(
+            title: tab.title,
+            image: UIImage(named: tab.imageName),
+            selectedImage: UIImage(named: tab.imageName)
+        )
+        
+        navigationController.tabBarItem.tag = tab.rawValue
+        navigationController.navigationBar.isHidden = (viewController is TrackerViewController) ? false : true
+        return navigationController
     }
 }
