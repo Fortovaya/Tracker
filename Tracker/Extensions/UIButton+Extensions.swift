bfff2e59a2ba2350225fe59e4c493e6783f05120
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-04-29 18:36:39 +0300

    Добавлено расширение для UIButton

--- Tracker/Extensions/UIButton+Extensions.swift
+++ Tracker/Extensions/UIButton+Extensions.swift
@@ -12,23 +12,29 @@ extension UIButton {
         backgroundColor: UIColor = .ypBlack,
         titleColor: UIColor    = .ypWhite,
         cornerRadius: CGFloat  = 16,
-        height: CGFloat        = 60
+        height: CGFloat        = 60,
+        width: CGFloat        = 335,
+        target: Any?,
+        action: Selector
     ) -> UIButton {
         let button = UIButton(type: .custom)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: height).isActive = true
-
+        button.widthAnchor.constraint(equalToConstant: width).isActive = true
+        
         button.backgroundColor = backgroundColor
         button.layer.cornerRadius = cornerRadius
-
+        
         button.setTitle(title, for: .normal)
         button.setTitleColor(titleColor, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
-
+        
         button.contentHorizontalAlignment = .center
         button.contentVerticalAlignment   = .center
         button.titleLabel?.textAlignment  = .center
-
+        
+        button.addTarget(target, action: action, for: .touchUpInside)
+        
         return button
     }
 }
