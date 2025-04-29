ea46eb9e1e94cfb5aa1ddce0b8c102c0d3ab1777
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-04-29 13:29:43 +0300

    Сверстан NavigationBar в TrackerViewController

--- Tracker/Trakers/TrackerViewController.swift
+++ Tracker/Trakers/TrackerViewController.swift
@@ -2,14 +2,135 @@
 //  TrackerViewController.swift
 //  Tracker
 //
-//  Created by Алина on 19.04.2025.
+//  Created by Алина on 20.04.2025.
 //
 import UIKit
 
 final class TrackerViewController: UIViewController {
+    //MARK: Private variable
+    private var categories: [TrackerCategory] = [] // список категорий и трекеров
+    private var completedTrackers: [TrackerRecord] = [] // выполненные трекеры (после нажатия на +, добавляется запись в completedTrackers
+    private var newTrackers: [Tracker] = []
     
-    // TO DO:
+    private lazy var dizzyImage: UIImageView = {
+        let image = UIImage(named: "dizzy")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isHidden = false
+        return imageView
+    }()
+    
+    private lazy var addTrackerButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "plus"), for: .normal)
+        button.addTarget(self, action: #selector(tapAddTrackerButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private lazy var dateButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle(DateFormatter.dateFormatter.string(from: Date()), for: .normal)
+        button.setTitleColor(.ypBlack, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        button.backgroundColor = .ypDatePicker
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.addTarget(self, action: #selector(tapDateButton), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: 77),
+            button.heightAnchor.constraint(equalToConstant: 34)
+        ])
+        
+        return button
+    }()
+    
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        let searchTextField = searchController.searchBar.searchTextField
+        
+        searchTextField.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        searchTextField.textColor = .ypBlack
+        
+        searchTextField.leftView?.tintColor = .ypGray
+        
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.layer.masksToBounds = true
+        
+        searchTextField.attributedPlaceholder = NSAttributedString(
+            string: "Поиск",
+            attributes: [
+                .foregroundColor: UIColor.ypGray,
+                .font: UIFont.systemFont(ofSize: 17, weight: .regular)
+            ]
+        )
+        
+        return searchController
+    }()
+    
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureConstraintsTrackerViewController()
+        hideKeyboardWhenTappedAround()
+        setupTopNavigationBar()
+        
+        definesPresentationContext = true
     }
+    
+    //MARK: Private method
+    private func configureConstraintsTrackerViewController() {
+        view.addSubview(dizzyImage)
+        
+        [dizzyImage].disableAutoresizingMask()
+        
+        NSLayoutConstraint.activate([
+            dizzyImage.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            dizzyImage.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+        ])
+    }
+    
+    @objc private func tapAddTrackerButton() {
+        // TO DO:
+        print("TO DO: tapAddTrackerButton()")
+    }
+    
+    @objc private func tapDateButton() {
+        let calendarVC = CalendarViewController()
+        calendarVC.modalPresentationStyle = .overCurrentContext
+        calendarVC.modalTransitionStyle = .crossDissolve
+        
+        calendarVC.onDatePicked = { [weak self] selectedDate in
+            guard let self = self else { return }
+            
+            let title = DateFormatter.dateFormatter.string(from: selectedDate)
+            self.dateButton.setTitle(title, for: .normal)
+            self.dismiss(animated: true)
+            print("Выбрана дата: \(title)")
+        }
+        present(calendarVC, animated: true)
+    }
+    
+    private func setupTopNavigationBar() {
+        title = "Трекеры"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        let leftItemButton = UIBarButtonItem(customView: addTrackerButton)
+        navigationItem.leftBarButtonItem = leftItemButton
+        
+        let rightItemButton = UIBarButtonItem(customView: dateButton)
+        navigationItem.rightBarButtonItem = rightItemButton
+    }
+    
 }
