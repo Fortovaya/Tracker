9db3ee8e943dad2bd5aae3fe593703544f1fd2b3
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-05-08 00:32:37 +0300

    Добавлены новые элементы в Color+Extensions

--- Tracker/Helpers/Extensions/Color+Extensions.swift
+++ Tracker/Helpers/Extensions/Color+Extensions.swift
@@ -16,4 +16,24 @@ extension UIColor {
     static var ypBackgroundTF: UIColor {UIColor(named: "#E6E8EB4D(30%)") ?? UIColor.lightGray }
     static var ypRed: UIColor{UIColor(named: "#F56B6C") ?? UIColor.red }
     static var ypToggle: UIColor{UIColor(named: "#E6E8EB") ?? UIColor.lightGray }
+    static var backgroundNight: UIColor{UIColor(named: "#414141D9(85%)") ?? UIColor.gray }
+    
+    static var ypCellColorRed: UIColor { UIColor(named: "#FD4C49") ?? UIColor.red }
+    static var ypCellColorOrange: UIColor { UIColor(named: "#FF881E") ?? UIColor.orange }
+    static var ypCellColorBlue: UIColor { UIColor(named: "#007BFA") ?? UIColor.blue }
+    static var ypCellColorPurple: UIColor { UIColor(named: "#6E44FE") ?? UIColor.purple }
+    static var ypCellColorGreen: UIColor { UIColor(named: "#33CF69") ?? UIColor.green }
+    static var ypCellColorPink: UIColor { UIColor(named: "#E66DD4") ?? UIColor.systemPink }
+    static var ypCellColorLightPink: UIColor { UIColor(named: "#F9D4D4") ?? UIColor.systemPink }
+    static var ypCellColorLightBlue: UIColor { UIColor(named: "#34A7FE") ?? UIColor.systemBlue }
+    static var ypCellColorMint: UIColor { UIColor(named: "#46E69D") ?? UIColor.systemTeal }
+    static var ypCellColorDarkBlue: UIColor { UIColor(named: "#35347C") ?? UIColor.systemIndigo }
+    static var ypCellColorCoral: UIColor { UIColor(named: "#FF674D") ?? UIColor.systemOrange }
+    static var ypCellColorBabyPink: UIColor { UIColor(named: "#FF99CC") ?? UIColor.systemPink }
+    static var ypCellColorPeach: UIColor { UIColor(named: "#F6C48B") ?? UIColor.systemOrange }
+    static var ypCellColorPeriwinkle: UIColor { UIColor(named: "#7994F5") ?? UIColor.systemBlue }
+    static var ypCellColorViolet: UIColor { UIColor(named: "#832CF1") ?? UIColor.systemPurple }
+    static var ypCellColorLavender: UIColor { UIColor(named: "#AD56DA") ?? UIColor.systemPurple }
+    static var ypCellColorLightPurple: UIColor { UIColor(named: "#8D72E6") ?? UIColor.systemPurple }
+    static var ypCellColorLime: UIColor { UIColor(named: "#2FD058") ?? UIColor.systemGreen }
 }
