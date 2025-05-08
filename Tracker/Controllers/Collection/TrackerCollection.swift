884b2a969eff5ab10217339a3c8ea2bf511a4f8e
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-05-08 19:08:43 +0300

    Добавлены расширения для TrackerCollection

---
+++ Tracker/Controllers/Collection/TrackerCollection.swift
@@ -0,0 +1,32 @@
+//
+//  TrackerCollection.swift
+//  Tracker
+//
+//  Created by Алина on 07.05.2025.
+//
+import UIKit
+
+final class TrackerCollection: NSObject {
+    
+   
+    
+  
+    
+}
+
+
+extension TrackerCollection: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension TrackerCollection: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+}
