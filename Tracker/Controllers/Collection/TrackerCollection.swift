5c6b22ac0360cdc1c4ed606ee8b237642a3b9bbf
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-05-11 02:37:59 +0300

    Реализована логика TrackerCollection

--- Tracker/Controllers/Collection/TrackerCollection.swift
+++ Tracker/Controllers/Collection/TrackerCollection.swift
@@ -69,7 +69,8 @@ final class TrackerCollection: NSObject {
 // MARK: - UICollectionViewDelegateFlowLayout
 extension TrackerCollection: UICollectionViewDelegateFlowLayout {
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets (top: 12, left: 0, bottom: 16, right: 0)
     }
     
@@ -79,12 +80,14 @@ extension TrackerCollection: UICollectionViewDelegateFlowLayout {
         return CGSize(width: width, height: width * 1.2)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
         
         return CGSize(width: collectionView.bounds.width, height: 40)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForFooterInSection section: Int) -> CGSize {
         
         return CGSize(width: collectionView.bounds.width, height: 58)
     }
