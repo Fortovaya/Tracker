cbfa8deff6b26ca847dbf3af1379f11a9c0af3b0
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-05-24 19:17:52 +0300

    Скорректирокан AppDelegate и SceneDelegate

--- Tracker/Application/SceneDelegate.swift
+++ Tracker/Application/SceneDelegate.swift
@@ -12,31 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
         
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = MainTabBarController()
         window?.makeKeyAndVisible()
-    }
-    
-    func sceneDidDisconnect(_ scene: UIScene) {
-        //nothing
-    }
-    
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        //nothing
-    }
-    
-    func sceneWillResignActive(_ scene: UIScene) {
-        //nothing
-    }
-    
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        //nothing
-    }
-    
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        //nothing
     }
 }
 
