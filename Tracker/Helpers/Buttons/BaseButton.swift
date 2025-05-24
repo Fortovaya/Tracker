b0f448816eaefa9bb8a6d63cfdae43c27affc70f
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-05-24 19:18:23 +0300

    Удалены лишние self при обращении к свойствам

--- Tracker/Helpers/Buttons/BaseButton.swift
+++ Tracker/Helpers/Buttons/BaseButton.swift
@@ -33,33 +33,33 @@ final class BaseButton: HighlightableButton {
     
     // MARK: - Private Methods
     private func setupLayout(_ height: CGFloat) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
     }
     
     private func setupAppearance(backgroundColor: UIColor,cornerRadius: CGFloat, borderColor: UIColor?,
                                  borderWidth: CGFloat) {
         self.backgroundColor = backgroundColor
-        self.layer.cornerRadius = cornerRadius
+        layer.cornerRadius = cornerRadius
         
         if let borderColor = borderColor {
-            self.layer.borderColor = borderColor.cgColor
-            self.layer.borderWidth = borderWidth
+            layer.borderColor = borderColor.cgColor
+            layer.borderWidth = borderWidth
         }
     }
     
     private func setupTitle(title: Resources.TitleButtons, titleColor: UIColor) {
-        self.setTitle(title.text, for: .normal)
-        self.accessibilityLabel = title.text
-        self.setTitleColor(titleColor, for: .normal)
-        self.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
-        self.contentHorizontalAlignment = .center
-        self.contentVerticalAlignment   = .center
-        self.titleLabel?.textAlignment  = .center
+        setTitle(title.text, for: .normal)
+        accessibilityLabel = title.text
+        setTitleColor(titleColor, for: .normal)
+        titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        contentHorizontalAlignment = .center
+        contentVerticalAlignment   = .center
+        titleLabel?.textAlignment  = .center
     }
     
     private func setupAction(target: Any?, action: Selector) {
-        self.addTarget(target, action: action, for: .touchUpInside)
+        addTarget(target, action: action, for: .touchUpInside)
     }
     
     // MARK: - required
