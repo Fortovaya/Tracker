861072492caf1c39711d600ff6bfac30f035884b
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-05-25 13:37:55 +0300

    Вынесена структура GeometricParams в отдельную сущность проекта

---
+++ Tracker/Models/GeometricParams.swift
@@ -0,0 +1,33 @@
+//
+//  GeometricParams.swift
+//  Tracker
+//
+//  Created by Алина on 24.05.2025.
+//
+
+import Foundation
+
+struct GeometricParams {
+    let cellCount: Int
+    let cellSpacing: CGFloat
+    let leftInset: CGFloat
+    let rightInset: CGFloat
+    let topInset:     CGFloat
+    let bottomInset:  CGFloat
+    
+    init(
+        cellCount: Int,
+        cellSpacing: CGFloat,
+        leftInset: CGFloat,
+        rightInset: CGFloat,
+        topInset: CGFloat,
+        bottomInset: CGFloat
+    ) {
+        self.cellCount = cellCount
+        self.cellSpacing = cellSpacing
+        self.leftInset = leftInset
+        self.rightInset = rightInset
+        self.topInset = topInset
+        self.bottomInset = bottomInset
+    }
+}
