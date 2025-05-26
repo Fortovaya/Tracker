0a78ce641ff52833053079eae1f5192bafc9a344
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-05-26 20:47:54 +0300

    Произведена регистрация DaysValueTransformer (Core Data stack)

--- Tracker/Application/AppDelegate.swift
+++ Tracker/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,38 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
+    
+    // MARK: UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tracker")
+        
+        DaysValueTransformer.register()
+        
+        container.loadPersistentStores { description, error in
+            if let error = error as NSError? {
+                fatalError("Не удалось загрузить Core Data store: \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    // MARK: — Core Data Saving support
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        guard context.hasChanges else { return }
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Ошибка сохранения контекста: \(nserror), \(nserror.userInfo)")
+        }
+    }
 }
-
