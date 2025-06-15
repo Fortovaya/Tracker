3b9db19a3a992f1644bf18b475cc49ef60ef84cf
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-06-15 21:06:37 +0300

    Произведено обновление тестовой инфраструктуры: добавлен SnapshotDevice

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -11,7 +11,10 @@ import SnapshotTesting
 
 final class TrackerTests: XCTestCase {
     
-    private static let testSnapshotSize = CGSize(width: 414, height: 896)
+    enum SnapshotDevice {
+        static let iPhone11 = CGSize(width: 414, height: 896)
+        static let iPhoneSE = CGSize(width: 320, height: 568)
+    }
     
     override class func setUp() {
         super.setUp()
@@ -19,37 +22,37 @@ final class TrackerTests: XCTestCase {
         UserDefaults.standard.synchronize()
     }
     
-    func testOnboardingFirstPage_iPhone11() {
+    func testOnboardingFirstPage() {
         let page = OnboardingPage(imageName: Resources.OnboardingImage.onBoardingBlue.imageName,
                                   titleText: NSLocalizedString("label.onBoardingBlue", comment: ""),
                                   index: 0,
                                   total: 2)
         
         let vc = OnboardingPageViewController(page: page)
-        assertSnapshot(of: vc, as: .image(size: Self.testSnapshotSize))
+        assertSnapshot(of: vc, as: .image(size: Self.SnapshotDevice.iPhone11))
     }
     
-    func testOnboardingSecondPage_iPhone11() {
+    func testOnboardingSecondPage() {
         let page = OnboardingPage(imageName: Resources.OnboardingImage.onBoardingRed.imageName,
                                   titleText: NSLocalizedString("label.onBoardingRed", comment: ""),
                                   index: 1, total: 2)
         
         let vc = OnboardingPageViewController(page: page)
-        assertSnapshot(of: vc, as: .image(size: Self.testSnapshotSize))
+        assertSnapshot(of: vc, as: .image(size: Self.SnapshotDevice.iPhone11))
     }
     
-    func testTrackerViewController_iPhone11(){
+    func testTrackerViewController(){
         let vc = TrackerViewController()
-        assertSnapshot(of: vc, as: .image(size: Self.testSnapshotSize))
+        assertSnapshot(of: vc, as: .image(size: Self.SnapshotDevice.iPhone11))
     }
     
-    func testTrackerTypeViewController_iPhone11(){
+    func testTrackerTypeViewController(){
         let vc = TrackerTypeViewController()
-        assertSnapshot(of: vc, as: .image(size: Self.testSnapshotSize))
+        assertSnapshot(of: vc, as: .image(size: Self.SnapshotDevice.iPhone11))
     }
     
-    func testScheduleViewController_iPhone11(){
+    func testScheduleViewController(){
         let vc = ScheduleViewController()
-        assertSnapshot(of: vc, as: .image(size: Self.testSnapshotSize))
+        assertSnapshot(of: vc, as: .image(size: Self.SnapshotDevice.iPhone11))
     }
 }
